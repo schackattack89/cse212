cb9503d635840b0647830ad15b9dff6bfc9c1f50
schackattack89 <nschack@gmail.com>
2025-01-10 15:06:46 -0500

    wk1 prove

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -36,7 +36,7 @@ public static class Arrays
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        for(int i=1; i<=amount; i++){               // loop to iterate the rotating swap for 'amount' times
+        for(int i=1; i<=amount; i++){               // loop to iterate the rotating swap for 'amount' times.
             int arrLen = data.Count;                // intantiated variable to be able to use nth term without data object calling a function of itself (i.e. data[data.Count])
             int lastCell = data[arrLen-1];          // placeholder for final value of 'data' in current iteration of the rotate algorithm
             for(int j=(data.Count)-1; j>0; j--){    // loop to iterate through each value of 'data'
